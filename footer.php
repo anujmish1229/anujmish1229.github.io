4f64b96c5fb899a3e1416f4a033ee58ced7a9dfd
anujmish1229 <anujmish1229@gmail.com>
2025-07-23 22:36:40 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
     <center>
-        <p>&copy; Copyright Senior Buddies 2024</p>
+        <p>&copy; Copyright Senior Buddies 2025<br>Developed by Anuj Mishra</p>
     </center>
 </footer>
 <script src="index.js"></script>
